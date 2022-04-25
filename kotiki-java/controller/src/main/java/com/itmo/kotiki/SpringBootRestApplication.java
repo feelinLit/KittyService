a60e8e75075088ca22fe8463b0dd5f2e7a345ee9
De-Bill <podshivalov.o29@gmail.com>
2022-04-25 15:37:46 +0300

    fix: remove test code

--- kotiki-java/controller/src/main/java/com/itmo/kotiki/SpringBootRestApplication.java
+++ kotiki-java/controller/src/main/java/com/itmo/kotiki/SpringBootRestApplication.java
@@ -19,21 +19,4 @@ public class SpringBootRestApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringBootRestApplication.class, args);
     }
-
-    @Bean
-    public CommandLineRunner initializeDb(PersonRepository personRepository, KittyRepository kittyRepository) {
-        return (args -> {
-            Person admin = new Person("admin_name", "admin", "admin", Role.ROLE_ADMIN, LocalDate.now(), null);
-            Person user1 = new Person("user1_name", "user1", "user1", Role.ROLE_USER, LocalDate.now(), null);
-            Person user2 = new Person("user2_name", "user2", "user2", Role.ROLE_USER, LocalDate.now(), null);
-            personRepository.saveAndFlush(admin);
-            personRepository.saveAndFlush(user1);
-            personRepository.saveAndFlush(user2);
-
-            Kitty kitty1 = new Kitty("MegaSuperNyash", "Sharik", Color.BLACK, LocalDate.now(), user1);
-            Kitty kitty2 = new Kitty("MegaSuperNyash", "Kvadratik", Color.BLACK, LocalDate.now(), user2);
-            kittyRepository.saveAndFlush(kitty1);
-            kittyRepository.saveAndFlush(kitty2);
-        });
-    }
 }
